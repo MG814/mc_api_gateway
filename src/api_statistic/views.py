c82e14f7503118561e0e17e359dfc53659f84c5a
MICHAŁ <mg814@wp.pl>
2025-05-03 11:50:23 +0200

    feat: add new features

---
+++ src/api_statistic/views.py
@@ -0,0 +1,19 @@
+import requests
+from django.http import JsonResponse, HttpResponseRedirect
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class StatisticGatewayView(APIView):
+
+    def post(self, request):
+        try:
+            response = requests.post(
+                f"http://web-statistic:8200/graphql/",
+                json=request.data
+            )
+            return Response(response.json(), status=response.status_code)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- src/core/urls.py
+++ src/core/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('api/', include('api_visits.urls')),
     path('api/', include('api_medical_records.urls')),
     path('api/', include('api_token_jwt.urls')),
+    path('api/', include('api_statistic.urls')),
 ]
